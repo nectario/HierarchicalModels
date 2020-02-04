870c826ea13ed23f308300b36e1a3a8b74b5bbcc
Nektarios Kalogridis <mail@nektarios.com>
2020-02-03 20:23:46 -0500

    Modifications

--- Classifiers.py
+++ Classifiers.py
@@ -29,22 +29,7 @@ def cnn_classifier():
 
     return model
 
-#input_1 = np.random.rand(3,3,3,1 ) #np.array([[1, 2, 3, 4, 5, 6, 7],[1, 2, 3, 4, 5, 6, 7],[1, 2, 3, 4, 5, 6, 7]])
 
-
-# (x_train, y_train), (x_test, y_test) = imdb.load_data(path="imdb.npz",
-#                                                       num_words=None,
-#                                                       skip_top=0,
-#                                                       maxlen=None,
-#                                                       seed=113,
-#                                                       start_char=1,
-#                                                       oov_char=2,
-#                                                       index_from=3)
-
-#X_train = pad_sequences(x_train, maxlen=1100, dtype='int32', padding='post', truncating='post', value=0.0)
-#X_test = pad_sequences(x_test, maxlen=1100, dtype='int32', padding='post', truncating='post', value=0.0)
-
-model = cnn_classifier()
 
 train_validation_split = tfds.Split.TRAIN.subsplit([8, 2])
 
@@ -53,10 +38,10 @@ train_validation_split = tfds.Split.TRAIN.subsplit([8, 2])
     split=(train_validation_split, tfds.Split.TEST),
     as_supervised=True)
 
-train_data = pad_sequences(train_data, maxlen=1100, dtype='int32', padding='post', truncating='post', value=0.0)
-test_data = pad_sequences(test_data, maxlen=1100, dtype='int32', padding='post', truncating='post', value=0.0)
+#train_data = pad_sequences(train_data, maxlen=1100, dtype='int32', padding='post', truncating='post', value=0.0)
+#test_data = pad_sequences(test_data, maxlen=1100, dtype='int32', padding='post', truncating='post', value=0.0)
 
-#model.fit(X_train, y_train, epochs=100, validation_data=(X_test,y_test), verbose=1)
+model = cnn_classifier()
 
 model.fit(train_data.shuffle(25000).batch(512),
                     epochs=20,
